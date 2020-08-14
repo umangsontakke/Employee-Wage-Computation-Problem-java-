4c7442c3cdae11d8ee2d880942f91d0188587820
umangsontakke <50995664+umangsontakke@users.noreply.github.com>
2020-08-14 21:06:29 +0530

    Update EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,9 +1,40 @@
 
-class EmployeeWage
+public class TotalWorkinghrsMonth
 {
-public static void main(String args[])
-{
-System.out.println("Welcome to EmployeeWage ComputationProgram on Master Branch");
-}
+
+public static final int IS_PART_TIME=1;
+public static final int IS_FULL_TIME=2;
+public static final int EMP_RATE_PER_HOUR=20;
+public static final int NUM_OF_WORKING_DAYS=2;
+public static final int MAX_HRS_IN_MONTH=10;
+	public static void main(String[] arg)
+	{
+                  //variables
+		int empHrs=0;
+		int totalempHrs=0;
+		int totalWorkingDays=0;
+	           //computation
+		while(totalEmpHrs<=MAX_HRS_IN_MONTH &&
+		totalworkingdays<NUM_OF_WORKING_DAYS)
+		{
+		totalWorkingDays++;
+                  int empCheck=(int)(Math.random()*10)%3;
+		switch(empCheck){
+		case IS_PART_TIME:
+			empHrs=4;
+			break;
+		case IS_FULL_TIME:
+			empHrs=8;
+			break;
+		default:
+			empHrs=0;
+	        }
+		
+		totalEmpWage+=empWage;
+		System.out.println("Day#:"+TotalWorkingDays+"Emp Hr:"+empHrs);
+        }
+		int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Total Emp Wage="+totalEmpWage);	
 
 }
+
